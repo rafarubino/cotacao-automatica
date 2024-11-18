93398b6d46648910c6ab2e971d7c7f40b7b6c97e
rafarubino <97356443+rafarubino@users.noreply.github.com>
2024-11-18 17:42:13 -0300

    formu.py

--- formu.py
+++ formu.py
@@ -12,19 +12,19 @@ RANGE_NAME = "fornecedores!A:D"  # altere o nome da aba se necessário
 # Função para obter as credenciais da conta de serviço a partir de st.secrets
 def init_google_sheets():
     google = {
-    "type:", st.secrets["type"]","
-     "project_id:", st.secrets["project_id"]","
-    "private_key_id:", st.secrets["private_key_id"]","
-    "private_key:", st.secrets["private_key"]","
-    "client_email:", st.secrets["client_email"]","
-    "client_id:", st.secrets["client_id"]","
-    "auth_uri:", st.secrets["auth_uri"]","
-    "token_uri:", st.secrets["token_uri"]","
-    "auth_provider_x509_cert_url:", st.secrets["auth_provider_x509_cert_url"]","
-    "client_x509_cert_url:", st.secrets["client_x509_cert_url"]
-}
-    creds_info = st.secrets["google"]  # Obtém o dicionário de credenciais de st.secrets
-    creds = Credentials.from_service_account_info(creds_info)
+        "type": st.secrets["type"],
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"],
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+    }
+    
+    creds = Credentials.from_service_account_info(google)
     return creds
 
 # Função para salvar os dados na planilha
