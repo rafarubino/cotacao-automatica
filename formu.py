f52b0330da9a85561eb2d3703f8fc68159dd0908
rafarubino <97356443+rafarubino@users.noreply.github.com>
2024-11-19 15:20:13 -0300

    Update formu.py

--- formu.py
+++ formu.py
@@ -59,6 +59,7 @@ def salvar_dados(produto, valor):
         st.write("Erro ao salvar na planilha:", err)  # Mensagem de erro
 
 # Código do Streamlit
+st.image("feriozzi_logo.png", width = 100)
 st.title("Formulário de Cotação")
 st.write("Este formulário tem o intuito de automatizar o processo de cotação")
 
